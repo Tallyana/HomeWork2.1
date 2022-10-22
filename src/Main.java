86f13befd059bf666907bbd706183f2714aa7ee4
Admin <iggla67@yandex.ru>
2022-10-22 21:59:37 +0400

    Изменение домашки на улучшенную версию

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,63 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!") ;
+        System.out.println("Задание 1") ;
 
-        var dog = 8.0 ;
-        System.out.println (dog);
+       var dog = 8.0 ;
+        System.out.println(dog);
         var cat =3.6 ;
         System.out.println(cat);
         var paper = 763789 ;
         System.out.println(paper);
-        var dog = dog+4.0;
-        System.out.println (dog);
-        var cat = cat+4.0;
-        System.out.println (cat);
-        var paper = paper +4.0 ;
-        System.out.println(paper);
-        var dog = dog- 3.5;
-        System.out.println (dog);
-        var cat = cat -1.6;
-        System.out.println (cat);
-        var paper = paper -7639 ;
-        System.out.println(paper);
-        var friend = 19;
-        System.out.println (friend);
-        var friend = friend+2;
-        System.out.println (friend);
-        var friend = friend/7 ;
-        System.out.println(friend);
-        var frog = 3.5;
-        System.out.println  (frog);
-        var frog = frog * 10;
-        System.out.println (frog);
-        var friend = frog/5 ;
-        System.out.println(frog);
-        var frog = frog +4.0 ;
-        System.out.println(frog);
-        var vesBoks1 = 78.2;
-        System.out.println  (vesBoks1);
-        var vesBoks2 = 82.7;
-        System.out.println (vesBoks2);
-        var myVmeste = vesBoks1 + vesBoks2 ;
-        System.out.println(myVmeste);
-        var myOtdelno = vesBoks2 – vesBoks1;
-        System.out.println(myOtdelno);
-        var vesBoks1 = 78.2;
-        System.out.println  (vesBoks1);
-        var vesBoks2 = 82.7;
-        System.out.println (vesBoks2);
-        var myVmeste = vesBoks1 + vesBoks2 ;
-        System.out.println(myVmeste);
-        var myOtdelno = vesBoks2 – vesBoks1;
-        System.out.println(myOtdelno);
-        var ostBoks = vesBoks2 % vesBoks1 ;
-        System . out.println("Один боец больше другого на“  + ostBoks +  “кг! “);
-        var kolvoChas =600 ;
-        System.out.println(kolvoChas);
-        var rabochDen1 = 8 ;
-        System.out.println(rabochDen1);
-        var kolvoLudey = kolvoChas/rabochDen1;
-        System.out.println(kolvoLudey);
-        System.out.println  ("Всего работников в компании –“ + kolvoLudey + '' человек".);
-        var dopolnitLudi = 94 ;
-        System.out.println(dopolnitludi);
-        var obshLudi = dopolnitLudi + kolvoLudey ;
-        System.out.println(obshLudi)
-        var rabochDen2 = kolvoChas /obshLudi ;
-        System.out.println(rabochDen2);
-        System.out.println ("Если в компании работает “ +  obshLudi  + ‘’человек, то всего ‘’+  rabochDen2 + ‘’часов работы может быть поделено между сотрудниками".);
 
+        dog = dog+4.0;
+        System.out.println (dog);
+         cat = cat+4.0;
+        System.out.println (cat);
+        paper = paper+4;
+        System.out.println(paper);
+
+        dog = dog- 3.5;
+        System.out.println (dog);
+        cat = cat -1.6;
+        System.out.println (cat);
+        paper = paper -7639 ;
+        System.out.println(paper);
+
+     System.out.println("Задание 2") ;
+     double friend = 19;
+     System.out.println(friend);
+     friend = friend+2;
+     System.out.println(friend);
+     friend = friend/7 ;
+     System.out.println(friend);
+
+     var frog = 3.5;
+     System.out.println(frog);
+     frog = frog * 10.0;
+     System.out.println (frog);
+     friend = (int) (frog/5.0);
+     System.out.println(frog);
+     frog = frog +4.0 ;
+     System.out.println(frog);
+
+     System.out.println("Задание 3") ;
+     var vesBokks1 = 78.2;
+     System.out.println(vesBokks1);
+      var vesBokss2 = 82.7;
+     System.out.println(vesBokss2);
+      var myVmeste = vesBokks1 + vesBokss2 ;
+     System.out.println(myVmeste);
+     var myOtdelno= vesBokss2 – vesBokks1;
+     System.out.println(myOtdelno);
+     vesBokks1 = 78.2;
+     System.out.println(vesBokks1);
+     vesBokss2 = 82.7;
+     System.out.println (vesBokss2);
+     myVmeste = vesBokks1 + vesBokss2 ;
+     System.out.println(myVmeste);
+     myOtdelno = vesBokss2 – vesBokks1;
+     System.out.println(myOtdelno);
+      var ostBoks = vesBokss2 % vesBokks1 ;
+     System . out. println(' Второй боец  больше другого на'  + ostBoks +  'кг! “);
     }
 }
